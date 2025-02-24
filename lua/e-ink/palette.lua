658debf2f3a8bfe95f9912d5de6f04ff2afdd53c
alexxGmZ <algilbertgomez@gmail.com>
2025-02-24 17:28:29 +0800

    refactor: rearrange palette

--- lua/e-ink/palette.lua
+++ lua/e-ink/palette.lua
@@ -1,36 +1,36 @@
 local M = {}
 
-M.monochrome = {
-   bg = "#CCCCCC",       -- Lightest
-   white = "#C2C2C2",    -- Light Gray
-   cyan1 = "#A4A4A4",    -- Soft Cyan
-   cyan2 = "#9A9A9A",    -- Deeper Cyan
-   yellow1 = "#B8B8B8",  -- Softer Yellow
-   yellow2 = "#AEAEAE",  -- Deeper Yellow
-   magenta1 = "#909090", -- Mid Magenta
-   magenta2 = "#868686", -- Darker Magenta
-   blue1 = "#7C7C7C",    -- Mid Blue
-   blue2 = "#727272",    -- Darker Blue
-   green1 = "#686868",   -- Mid Green
-   green2 = "#5E5E5E",   -- Darker Green
-   red1 = "#545454",     -- Dark Red
-   red2 = "#4A4A4A",     -- Darker Red
-   black1 = "#474747",   -- Deep Black
-   black2 = "#333333"    -- Darkest
+M.mono = {
+   "#CCCCCC",
+   "#C2C2C2",
+   "#B8B8B8",
+   "#AEAEAE",
+   "#A4A4A4",
+   "#9A9A9A",
+   "#909090",
+   "#868686",
+   "#7C7C7C",
+   "#727272",
+   "#686868",
+   "#5E5E5E",
+   "#545454",
+   "#4A4A4A",
+   "#474747",
+   "#333333"
 }
 
 M.everforest = {
-   red1 = "#f85552",
-   red2 = "#e66868",
+   red1 = "#F85552",
+   red2 = "#E66868",
    red3 = "#FFE7DE",
-   yellow1 = "#dfa000",
-   green1 = "#8da101",
-   green2 = "#93b259",
-   green3 = "#F3F5D9",
-   blue1 = "#3a94c5",
+   yellow1 = "#DFA000",
+   green1 = "#8DA101",
+   green2 = "#93B259",
+   green3 = "#f3f5d9",
+   blue1 = "#3A94C5",
    blue2 = "#ECF5ED",
-   cyan1 = "#35a77c",
-   magenta1 = "#df69ba"
+   cyan1 = "#35A77C",
+   magenta1 = "#DF69BA"
 }
 
 return M
